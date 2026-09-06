3a58f74c3e7288bd5e869f4e31abcac423848406
dolodorsey <dolodorsey@gmail.com>
2026-09-06 03:24:46 -0400

    Add GROWN-ISH social preview

---
+++ src/app/atlanta/grown-ish/opengraph-image.tsx
@@ -0,0 +1,3 @@
+import { makeOgImage, size, contentType } from "@/app/_components/OgCard";
+export { size, contentType };
+export default function Image(){return makeOgImage({kicker:"21+ CONCERT SERIES",title:"GROWN-ISH",subtitle:"ICONIC's recurring 21+ concert property at Southlake Arena."});}
