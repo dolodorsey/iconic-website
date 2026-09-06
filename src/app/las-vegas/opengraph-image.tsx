2ab1dd575220b978d41aceb4072d1b51d23e2b1c
dolodorsey <dolodorsey@gmail.com>
2026-09-06 03:25:12 -0400

    Add Las Vegas social preview

---
+++ src/app/las-vegas/opengraph-image.tsx
@@ -0,0 +1,3 @@
+import { makeOgImage, size, contentType } from "@/app/_components/OgCard";
+export { size, contentType };
+export default function Image(){return makeOgImage({kicker:"2027 NATIONAL CIRCUIT",title:"ICONIC LAS VEGAS",subtitle:"A dedicated ICONIC market identity for Las Vegas."});}
