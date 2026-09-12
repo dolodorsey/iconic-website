d21507c0dd9e06ecd7ab00f060ad004431f75ce9
dolodorsey <dolodorsey@gmail.com>
2026-09-12 04:17:40 -0400

    Load ICONIC UI-first polish

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ import "./iconic-refinement.css";
 import "./flagship-refinement.css";
 import "./company-pages.css";
 import "./final-audit.css";
+import "./ui-first-polish.css";
 import { SITE_URL } from "../lib/site-url";
 import Analytics from "./_components/Analytics";
 
