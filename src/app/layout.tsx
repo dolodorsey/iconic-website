8a2c12313587351a63077234078656f197a4198e
dolodorsey <dolodorsey@gmail.com>
2026-09-11 21:04:58 -0400

    style: load full ICONIC homepage system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import "./mobile-polish.css";
 import "./visual-overhaul.css";
 import "./visual-overhaul-responsive.css";
+import "./iconic-home.css";
 import { SITE_URL } from "../lib/site-url";
 import Analytics from "./_components/Analytics";
 
