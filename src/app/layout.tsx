9881e192b64ba4e0bc4bc6bc1b375dfb84df8e60
dolodorsey <dolodorsey@gmail.com>
2026-09-11 22:30:04 -0400

    style: load final responsive QA polish across ICONIC

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import "./visual-overhaul.css";
 import "./visual-overhaul-responsive.css";
 import "./iconic-home.css";
 import "./platform-shell.css";
+import "./qa-polish.css";
 import { SITE_URL } from "../lib/site-url";
 import Analytics from "./_components/Analytics";
 
