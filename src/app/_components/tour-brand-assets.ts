6913fcbc38a55e23718d8e767a2e61e8a1a23d71
dolodorsey <dolodorsey@gmail.com>
2026-09-11 20:49:49 -0400

    feat: add official Summer Walker and DJ Snake tour brand assets

---
+++ src/app/_components/tour-brand-assets.ts
@@ -0,0 +1,5 @@
+export const SUMMER_WALKER_SOUL_SYMPHONY_LOGO = "https://cdn.shopify.com/s/files/1/0759/7506/5791/files/iconic-summer-walker-soul-symphony-logo.png?v=1789174097";
+
+export const DJ_SNAKE_AND_FRIENDS_LOGO = "https://cdn.shopify.com/s/files/1/0759/7506/5791/files/iconic-dj-snake-and-friends-logo.png?v=1789174105";
+
+export const PARDON_MY_FRENCH_TOUR_LOGO = "https://cdn.shopify.com/s/files/1/0759/7506/5791/files/iconic-pardon-my-french-tour-logo.png?v=1789174112";
